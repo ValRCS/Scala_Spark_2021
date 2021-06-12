f70fce97028ffc469e8d2d55f58841abaeaec814
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-06-12 14:20:45 +0300

    added master option to createSpark

--- src/main/scala/com/github/valrcs/spark/SparkUtil.scala
+++ src/main/scala/com/github/valrcs/spark/SparkUtil.scala
@@ -3,10 +3,10 @@ import org.apache.spark.sql.SparkSession
 
 object SparkUtil {
   //TODO docstrings
-  def createSpark(appName:String, verbose:Boolean = true): SparkSession = {
+  def createSpark(appName:String, verbose:Boolean = true, master: String= "local"): SparkSession = {
     if (verbose) println(s"$appName with Scala version: ${util.Properties.versionNumberString}")
 
-    val spark = SparkSession.builder().appName(appName).master("local").getOrCreate()
+    val spark = SparkSession.builder().appName(appName).master(master).getOrCreate()
     spark.conf.set("spark.sql.shuffle.partitions", "5") //recommended for local, default is 200?
     if (verbose) println(s"Session started on Spark version ${spark.version}")
     spark
