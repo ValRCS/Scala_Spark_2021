b53d988d37016d91e34dfe1acf953fdc8aaccae1
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-06-14 21:58:25 +0300

    Spark select and selectExpr methods

--- src/main/scala/com/github/valrcs/spark/BasicStructuredOperations.scala
+++ src/main/scala/com/github/valrcs/spark/BasicStructuredOperations.scala
@@ -102,9 +102,122 @@ object BasicStructuredOperations extends App {
   //in fact for creating multiple columns at once it is recommended to use SQL select syntax
   //https://sparkbyexamples.com/spark/spark-dataframe-withcolumn#add-replace-update-multiple-columns
 
-  //TODO exercise
-  //TODO create a new Dataframe which has ALL columns from properDf  and also newPrice which is 10% higher than existing price
-  //TODO create a Dataframe of all columns above Dataframe with only rows where price is less than 1.00
   //use mydf.show() to show end results
   //remember you need to create a temporary view for properDF first!!
+
+  properDf.createOrReplaceTempView("pdfView")
+  val myDf = spark.sql("SELECT *, ROUND((price * 1.1),2) as bigPrice FROM pdfView")
+  myDf.show()
+
+  myDf.createOrReplaceTempView("newPdfView")
+  val myNewDf = spark.sql("SELECT * FROM newPdfView WHERE price < 1.00")
+  myNewDf.show()
+  //if I just want to see results I do not need to save into Dataframe
+  spark.sql("SELECT * FROM newPdfView WHERE bigPrice < 1.00").show()
+
+  //we can also get our column names as Array of Strings
+  println(myNewDf.columns.mkString("Array(", ", ", ")")) //could use foreach as well
+
+  //first row
+  val firstRow = myNewDf.first() //head also should work
+  println(firstRow)
+  println(firstRow.mkString("Row(", ", ", ")"))
+  println(firstRow.get(2)) //should give us potato
+  println(myNewDf.head.get(2)) //should also be a potato
+
+  //Creating Rows
+  //You can create rows by manually instantiating a Row object with the values that belong in each
+  //column. It’s important to note that only DataFrames have schemas. Rows themselves do not have
+  //schemas. This means that if you create a Row manually, you must specify the values in the same
+  //order as the schema of the DataFrame to which they might be appended
+
+  // in Scala
+  import org.apache.spark.sql.Row
+  //we are creating a standalone Row with no relation to anything just yet
+  val myRow = Row(300000, "XC90", "Volvo", 10000, 2000, Seq("Centrs","Jurmala"), true, 12500)
+  // in Scala
+  println(myRow(0))// type Any
+  println(myRow(0).toString) //this casts Any to Scala String type, will work with pretty much anything
+//  println(myRow(0).asInstanceOf[String]) // String coerciion out of Int will throw an int
+//  println(myRow.getString(0) )// String also does not work on non STrings
+  println(myRow.getInt(3)) // Int
+
+  //TODO println(myRow.json) //myRow does not know about the column names //we need schema for this
+
+
+  // in Scala
+  //it is possible to create a DataFrame out of sequence of Rows and manual Schema
+  import org.apache.spark.sql.types.{StructField, StructType, StringType, LongType}
+  val myManualSchema2 = new StructType(Array(
+    new StructField("some", StringType, true),
+    new StructField("col", StringType, true),
+    new StructField("names", LongType, false)))
+  val myRows = Seq(Row("Hello", null, 1L), Row("There", "something", 20L))
+  val myRDD = spark.sparkContext.parallelize(myRows)
+  val myDfFromRows = spark.createDataFrame(myRDD, myManualSchema2) //funnily enough it works also on myManualSchema which is similar
+  myDfFromRows.show()
+
+//  select and selectExpr
+//  select and selectExpr allow you to do the DataFrame equivalent of SQL queries on a table of
+//    data:
+
+  //in SQL SELECT DEST_COUNTRY_NAME FROM dfTable LIMIT 2
+  fdf.select("DEST_COUNTRY_NAME").show(2)
+
+  // in Scala //show is just for printing (returns nothing Unit)
+  //-- in SQL
+  //SELECT DEST_COUNTRY_NAME, ORIGIN_COUNTRY_NAME FROM dfTable LIMIT 2
+  fdf.select("DEST_COUNTRY_NAME", "ORIGIN_COUNTRY_NAME").show(2)
+
+  // you can refer to columns in a number of different
+  //ways; all you need to keep in mind is that you can use them interchangeably
+  // in Scala NOT good in practice to use different methods
+  //stick with one + sql one
+  import org.apache.spark.sql.functions.{expr, col, column}
+  fdf.select(
+    fdf.col("DEST_COUNTRY_NAME"),
+    col("DEST_COUNTRY_NAME"),
+    column("DEST_COUNTRY_NAME"),
+    'DEST_COUNTRY_NAME, //shortest one, but speed wise they are all the same
+    $"DEST_COUNTRY_NAME",
+    expr("DEST_COUNTRY_NAME")) //we have a new Dataframe of 6 columns
+    .show(2)
+
+  //As we’ve seen thus far, expr is the most flexible reference that we can use. It can refer to a plain
+  //column or a string manipulation of a column. To illustrate, let’s change the column name, and
+  //then change it back by using the AS keyword and then the alias method on the column:
+  //// in Scala
+  fdf.select(expr("DEST_COUNTRY_NAME AS destination")).show(2)
+
+//  This changes the column name to “destination.” You can further manipulate the result of your
+//    expression as another expression:
+    // in Scala
+  //here destination is overwritten immediately with a new alias
+    fdf.select(expr("DEST_COUNTRY_NAME as destination").alias("AGAIN_DEST_COUNTRY_NAME"))
+    .show(2)
+  fdf.select(expr("DEST_COUNTRY_NAME").alias("NEW_DEST_COUNTRY_NAME"))
+    .show(2)
+
+//  Because select followed by a series of expr is such a common pattern, Spark has a shorthand
+//  for doing this efficiently: selectExpr. This is probably the most convenient interface for
+//    everyday use:
+  // in Scala
+  fdf.selectExpr("DEST_COUNTRY_NAME as newColumnName", "DEST_COUNTRY_NAME").show(2)
+
+  //We can treat selectExpr as a simple way to build up
+  //complex expressions that create new DataFrames. In fact, we can add any valid NON-AGGREGATING
+  //SQL statement, and as long as the columns resolve, it will be valid! Here’s a simple example that
+  //adds a new column withinCountry to our DataFrame that specifies whether the destination and
+  //origin are the same:
+
+  // in Scala
+  fdf.selectExpr(
+    "*", // include all original columns
+    "(DEST_COUNTRY_NAME = ORIGIN_COUNTRY_NAME) as withinCountry") //this will be a boolean column
+    .show(10)
+
+  // in Scala
+  //With select expression, we can also specify aggregations over the entire DataFrame
+  fdf.selectExpr("avg(count)", "count(distinct(DEST_COUNTRY_NAME))").show(2)
+  //TODO produce purse SQL version of the above line (we already have most of the syntax place just need a view
 }
