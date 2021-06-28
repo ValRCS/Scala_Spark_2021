77f55a33d88efe8899b8042055cb212dc6dd55bb
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-06-28 20:05:39 +0300

    Exercise to read some sales data create array data type

---
+++ src/main/scala/com/github/valrcs/spark/ExerciseJ28.scala
@@ -0,0 +1,12 @@
+package com.github.valrcs.spark
+
+object ExerciseJ28 extends App {
+  //TODO read 28th of Jun 2011 sales data
+  //TODO add new column descWords with description words split into Array
+  //TODO add another column hasRed which indicates whether there is RED among the words
+  //TODO add countWords column
+  //TODO show first 10 rows ordered by countWords
+
+  //Bonus
+  //TODO explode the descWords column
+}
