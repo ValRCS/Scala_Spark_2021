c5d7bf501621d5ce4a762ac9673716a954bd8122
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-06-29 19:17:57 +0300

    UDF stub

---
+++ src/main/scala/com/github/valrcs/spark/Ch6UDF.scala
@@ -0,0 +1,16 @@
+package com.github.valrcs.spark
+
+object Ch6UDF extends App {
+  println("Creating our own UDFs - user defined functions")
+
+  val spark = SparkUtil.createSpark("ch6")
+
+  // in Scala
+  val df = spark.read.format("csv")
+    .option("header", "true")
+    .option("inferSchema", "true") //so Spark tries to figure out the schema if column has 1 2 and 3C the type will be StringType
+    .load("./src/resources/retail-data/by-day/2010-12-01.csv")
+
+  df.printSchema()
+  df.createOrReplaceTempView("dfTable")
+}
