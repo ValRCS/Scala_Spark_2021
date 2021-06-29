4de40e37005a94619c1a9fc65e2e01c848fe650e
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-06-29 21:07:30 +0300

    UDF exercise solved

--- src/main/scala/com/github/valrcs/spark/ExerciseJ29.scala
+++ src/main/scala/com/github/valrcs/spark/ExerciseJ29.scala
@@ -1,5 +1,7 @@
 package com.github.valrcs.spark
 
+import org.apache.spark.sql.functions.{col, desc, expr, round, udf}
+
 object ExerciseJ29 extends App {
   //TODO read June 29th sales data for 2011
   //TODO create your own UDF - which calculates total from Quantity and UnitPrice
@@ -9,4 +11,23 @@ object ExerciseJ29 extends App {
 
   //TODO bonus, create total2 column which you can use regular col math
   //because for calculating two column multiplication you can live without UDF :)
+  val spark = SparkUtil.createSpark("ch6")
+
+  // in Scala
+  val df = spark.read.format("csv")
+    .option("header", "true")
+    .option("inferSchema", "true") //so Spark tries to figure out the schema if column has 1 2 and 3C the type will be StringType
+    .load("./src/resources/retail-data/by-day/2011-06-29.csv")
+
+  df.printSchema()
+  df.describe().show(false)
+  def total(quantity:Int, unitPrice:Double):Double = quantity * unitPrice
+  spark.udf.register("total", total(_:Integer, _:Double):Double)
+  val totalUdf = udf(total(_:Integer, _:Double):Double)
+
+  df.withColumn("Total", round(totalUdf(col("Quantity"), col("UnitPrice")), 2))
+    .withColumn("Total2", round(col("Quantity")* col("UnitPrice"), 2))
+    .withColumn("Total3", expr("total(Quantity, unitPrice)"))
+    .orderBy(desc("Total"))
+    .show(10, false)
 }
