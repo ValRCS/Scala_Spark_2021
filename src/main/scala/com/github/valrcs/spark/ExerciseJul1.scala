865c4b9fc3f5a6d1c08e312690ac66bad46347d2
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-07-01 20:08:46 +0300

    Daily grouping exercise

---
+++ src/main/scala/com/github/valrcs/spark/ExerciseJul1.scala
@@ -0,0 +1,14 @@
+package com.github.valrcs.spark
+
+object ExerciseJul1 extends App {
+  //TODO read data from July 1st of 2011 CSV
+  //TODO find what is the correlation coefficient for UnitPrice and Quantity (should be close to 0)
+
+  //TODO find top 10 countries that purchased items that day
+  //TODO find count of items purchased in each invoice
+  //TODO find average quantity of items purchased per invoice
+  //TODO find sum of all items purchases per invoice
+
+  //Bonus TODO Find Top 5 biggest purchases per invoice that day - you will need a total column first before grouping
+
+}
