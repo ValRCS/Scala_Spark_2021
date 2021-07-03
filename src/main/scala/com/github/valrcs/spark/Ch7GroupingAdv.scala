010e88299905bf5b35698389ecebeefd93d64afa
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-07-03 14:04:24 +0300

    Pivot and custom aggregation function example

--- src/main/scala/com/github/valrcs/spark/Ch7GroupingAdv.scala
+++ src/main/scala/com/github/valrcs/spark/Ch7GroupingAdv.scala
@@ -1,7 +1,8 @@
 package com.github.valrcs.spark
 
-import org.apache.spark.sql.functions
-import org.apache.spark.sql.functions.{col,count, desc, expr, grouping_id, sum, to_date}
+import org.apache.spark.sql
+import org.apache.spark.sql.{Dataset, Row, functions}
+import org.apache.spark.sql.functions.{asc, col, count, desc, expr, grouping_id, sum, to_date}
 
 object Ch7GroupingAdv extends App {
   val spark = SparkUtil.createSpark("ch7")
@@ -131,4 +132,126 @@ object Ch7GroupingAdv extends App {
   //get top 5 sales(which is sum(Total)) for each single grouping of the following columns
   //"customerId", "stockCode", "Date", "Country"
   //use dfNoNull for your aggregations
+
+  dfNoNull
+    .groupBy("customerID")
+    .agg(
+      sum("Quantity"),
+      sum("Total"),
+      count("Total")
+    )
+    .orderBy(desc("sum(Total)"))
+    .show(5, false)
+
+  //since we are lazy programmers we want to avoid copy and pasting multiple single group queries
+  //so we will make a function to help us along we will give a dataframe and grouping column
+  //and we will receive a aggregated dataframe
+  //requirement being that Datafmae has Quantity and Total column names and obviously the groupColName as well
+  def topSalesBy(df: sql.DataFrame, groupColName: String): Dataset[Row] = {
+    df
+      .groupBy(groupColName)
+      .agg(
+        sum("Quantity"),
+        sum("Total"),
+        count("Total")
+      )
+      .orderBy(desc("sum(Total)"))
+  }
+
+  val colGroups = Seq("customerId", "stockCode", "Date", "Country")
+
+  //if I wanted to save them I could do map, but then of course memory could become an issue for really large data sets.
+  //instead i will use foreach to show the results
+  colGroups.foreach(colName => topSalesBy(dfNoNull, colName).show(5, false))
+
+//  Pivot
+//  Pivots make it possible for you to convert a row into a column. For example, in our current data
+//    we have a Country column. With a pivot, we can aggregate according to some function for each
+//  of those given countries and display them in an easy-to-query way
+
+  // in Scala
+  //without Pivot
+  dfWithDate
+    .groupBy("date", "Country")
+    .agg(
+      sum("Quantity"),
+      sum("UnitPrice"),
+      sum("CustomerID"), //of course CustomerIDs do not make sense to add usually :)
+      sum("Total")
+    ) //summing countries does not really work, we get null
+    .orderBy(asc("date"))
+    .show(5, false)
+
+  //with Pivot so making lots of new columns for each country and sum combination
+  val pivoted = dfWithDate
+    .groupBy("date")
+    .pivot("Country") //so we moved a row of country values into columns, useful when you do not have too many distinct row
+    .sum()
+    .orderBy(asc("date")) //order AFTER aggregation
+
+  pivoted.show(5,false)
+
+  //so again Pivot is quite nie for exploring data when you do not have too many distinct values in pivot column
+  //if you have 1000 different values you will get 1000 * number of aggregation columns
+
+
+  //User-Defined Aggregation Functions
+  //User-defined aggregation functions (UDAFs) are a way for users to define their own aggregation
+  //functions based on custom formulae or business rules. You can use UDAFs to compute custom
+  //calculations over groups of input data (as opposed to single rows). Spark maintains a single
+  //AggregationBuffer to store intermediate results for every group of input data.
+  //To create a UDAF, you must inherit from the UserDefinedAggregateFunction base class and
+  //implement the following methods:
+  //inputSchema represents input arguments as a StructType
+  //bufferSchema represents intermediate UDAF results as a StructType
+  //dataType represents the return DataType
+  //deterministic is a Boolean value that specifies whether this UDAF will return the
+  //same result for a given input
+  //initialize allows you to initialize values of an aggregation buffer
+  //update describes how you should update the internal buffer based on a given row
+  //merge describes how two aggregation buffers should be merged
+  //evaluate will generate the final result of the aggregation
+  //The following example implements a BoolAnd, which will inform us whether all the rows (for a
+  //given column) are true; if they’re not, it will return false:
+
+  import org.apache.spark.sql.expressions.MutableAggregationBuffer
+  import org.apache.spark.sql.expressions.UserDefinedAggregateFunction //depreceated so will need to read on new method eventually
+  import org.apache.spark.sql.types._
+  class BoolAnd extends UserDefinedAggregateFunction {
+    def inputSchema: org.apache.spark.sql.types.StructType =
+      StructType(StructField("value", BooleanType) :: Nil)
+    def bufferSchema: StructType = StructType(
+      StructField("result", BooleanType) :: Nil
+    )
+    def dataType: DataType = BooleanType
+    def deterministic: Boolean = true
+    def initialize(buffer: MutableAggregationBuffer): Unit = {
+      buffer(0) = true
+    }
+    def update(buffer: MutableAggregationBuffer, input: Row): Unit = {
+      buffer(0) = buffer.getAs[Boolean](0) && input.getAs[Boolean](0)
+    }
+    def merge(buffer1: MutableAggregationBuffer, buffer2: Row): Unit = {
+      buffer1(0) = buffer1.getAs[Boolean](0) && buffer2.getAs[Boolean](0)
+    }
+    def evaluate(buffer: Row): Any = {
+      buffer(0)
+    }
+  }
+
+  //Now, we simply instantiate our class and/or register it as a function:
+  val ba = new BoolAnd
+  spark.udf.register("booland", ba) //again depreceated eventally will need updating
+
+  spark.range(1)
+    .selectExpr("explode(array(TRUE, TRUE, TRUE)) as tcol") //so 3 here
+    .selectExpr("explode(array(TRUE, FALSE, TRUE)) as fcol", "tcol") //so 3 x 3 here since we took tcol with 3 and then exploded
+    .show(10, false) //so 9 rows actually
+
+
+  spark.range(1)
+    .selectExpr("explode(array(TRUE, TRUE, TRUE)) as t")
+    .selectExpr("explode(array(TRUE, FALSE, TRUE)) as f", "t")
+    .select(ba(col("t")), expr("booland(f)"))
+    .show()
 }
