dc55be410ca026fa92713383270ae38b3f58bbfc
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-07-06 21:08:34 +0300

    Exercise for Jul 6 solved

--- src/main/scala/com/github/valrcs/spark/ExerciseJul6.scala
+++ src/main/scala/com/github/valrcs/spark/ExerciseJul6.scala
@@ -1,10 +1,25 @@
 package com.github.valrcs.spark
 
-object ExerciseJul6 {
+object ExerciseJul6 extends App {
   //TODO read parquet files folder from src/resources/regression
   //TODO print schema
   //TODO show a sample of rows
   //TODO show some statistics
 
   //possibly something did not commit to git
+  val spark = SparkUtil.createSpark("exJul06")
+
+//  val filePath = "./src/resources/regression/*.parquet" //using wildcard is certainly fine
+  val filePath = "./src/resources/regression" //we could also just supply the folder with the parquet files
+
+  val parquetDF = spark
+    .read
+    .format("parquet")
+    .load(filePath)
+
+  parquetDF.printSchema()
+  parquetDF.describe().show()
+  parquetDF.show(10,false)
+  //turns out we only have 5 rows out of 7 parque files (in gzip format)
+  //it could happen but usually you would expect more rows
 }
