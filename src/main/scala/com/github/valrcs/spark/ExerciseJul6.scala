54b19d72783386cbfca53462b523b57cec4ce61e
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-07-06 20:06:08 +0300

    Exercise for Jul 6

---
+++ src/main/scala/com/github/valrcs/spark/ExerciseJul6.scala
@@ -0,0 +1,10 @@
+package com.github.valrcs.spark
+
+object ExerciseJul6 {
+  //TODO read parquet files folder from src/resources/regression
+  //TODO print schema
+  //TODO show a sample of rows
+  //TODO show some statistics
+
+  //possibly something did not commit to git
+}
