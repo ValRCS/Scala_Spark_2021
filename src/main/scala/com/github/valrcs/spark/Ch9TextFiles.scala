f35b4b3595b5d768531a11a53bd3b71a9a3ff062
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-07-08 20:07:31 +0300

    reading and writing text files

--- src/main/scala/com/github/valrcs/spark/Ch9TextFiles.scala
+++ src/main/scala/com/github/valrcs/spark/Ch9TextFiles.scala
@@ -1,6 +1,7 @@
 package com.github.valrcs.spark
 
-import org.apache.spark.sql.functions.{col, expr, regexp_extract}
+import org.apache.spark.sql.functions
+import org.apache.spark.sql.functions.{col, expr, lit, regexp_extract}
 
 object Ch9TextFiles extends App {
   val spark = SparkUtil.createSpark("ch9DataSources")
@@ -22,5 +23,42 @@ object Ch9TextFiles extends App {
 
   //TODO create column called name, which extracts name, - name being defined as something after first comma which starts with a capital Letter
 
+  val nameRegex = "\\d,\\s*([A-Z]{1}[a-z]*)"
+  val carRegex = ",.*,\\s*([A-Z].*)," //so , anything , then possible whitespace and then Car with anything until 3rd comma
 
+  df
+    .withColumn("nameAndAll", regexp_extract(col("value"), nameRegex, 0))
+    .withColumn("name", regexp_extract(col("value"), nameRegex, 1))
+    .withColumn("car", regexp_extract(col("value"), carRegex, 1))
+
+    .show(false)
+
+  val ndf = df
+    .withColumn("name", regexp_extract(col("value"), nameRegex, 1))
+    .withColumn("car", regexp_extract(col("value"), carRegex, 1))
+    .withColumn("year", regexp_extract(col("value"), regexString, 1))
+
+  ndf.show(false)
+
+  //now of course it would be beneficial to store the ndf as parquet or csv or some other stronger structure format
+
+  ndf
+    .select(col("car"))
+    .na
+    .drop //so we drop only nulls from car column
+    .filter("car != ''") //only non empty car strings
+    .write
+    .mode("overwrite")
+    .text("./src/resources/cars.txt")
+
+  //so how about saving multiple columns in a text file
+  //well we can create a new text column with all the other columns
+  //again saving it as better structured date would be better
+
+  ndf
+    .filter("car != '' AND name != ''")
+    .select(functions.concat(col("name"), lit(" "), col("year"), lit(":::"), col("car")))
+    .write
+    .mode("overwrite")
+    .text("./src/resources/dirty-data.txt")
 }
