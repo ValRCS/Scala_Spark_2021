949aa6ea216bd46d9bd7d6bddd15d4ce2b933020
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-07-08 19:05:30 +0300

    Reading Text Files with Spark stub

---
+++ src/main/scala/com/github/valrcs/spark/Ch9TextFiles.scala
@@ -0,0 +1,9 @@
+package com.github.valrcs.spark
+
+object Ch9TextFiles extends App {
+  val spark = SparkUtil.createSpark("ch9DataSources")
+  val filePath = "./src/resources/csv/dirty-data.csv"
+
+
+
+}
