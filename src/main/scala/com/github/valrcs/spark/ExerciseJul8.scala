7cd643abdeddefcdc18a735cd040bb7dfb29a8f0
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-07-08 20:10:19 +0300

    Jul 8 exercise

---
+++ src/main/scala/com/github/valrcs/spark/ExerciseJul8.scala
@@ -0,0 +1,8 @@
+package com.github.valrcs.spark
+
+object ExerciseJul8 extends App {
+  //TODO load 1920_Aspazija.txt with Spark(not regular Scala)
+
+  //TODO extract all titles of the poems
+  //TODO save the poem titles in a new text file(actually folder) called 1920_Aspazija_titles.txt
+}
