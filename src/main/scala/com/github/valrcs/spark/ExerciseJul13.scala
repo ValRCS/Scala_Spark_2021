dc4956c2ee2fe6129fe80a9a4fb1949cdaf0e477
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-07-13 20:08:15 +0300

    Create and Load model exercise

---
+++ src/main/scala/com/github/valrcs/spark/ExerciseJul13.scala
@@ -0,0 +1,15 @@
+package com.github.valrcs.spark
+
+object ExerciseJul13 extends App {
+
+  //TODO run and save model using code from Ch24Pipeline
+
+  //TODO load the model here
+  //TODO load into dataframe ml-data.csv
+
+  //TODO make a prediction(transform) on all values of dataframe (no need to split)
+
+  //TODO Show the results
+
+
+}
