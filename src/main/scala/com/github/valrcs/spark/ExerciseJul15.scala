67c3b54ed110b348ab0ce07cdba94bbb2ce76997
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-07-15 21:19:20 +0300

    Jul 15 exercise solved

--- src/main/scala/com/github/valrcs/spark/ExerciseJul15.scala
+++ src/main/scala/com/github/valrcs/spark/ExerciseJul15.scala
@@ -1,6 +1,10 @@
 package com.github.valrcs.spark
 
-import org.apache.spark.ml.classification.{ RandomForestClassifier,GBTClassifier,LinearSVC,NaiveBayes}
+import org.apache.spark.ml.classification.{GBTClassifier, LinearSVC, NaiveBayes, RandomForestClassifier}
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.feature.RFormula
+import org.apache.spark.ml.regression.RandomForestRegressor
+import org.apache.spark.sql.DataFrame
 
 object ExerciseJul15 extends App {
   //TODO load iris.data
@@ -12,4 +16,83 @@ object ExerciseJul15 extends App {
 
   //train on the train set
   //evaluate accuracy on the test set
+
+//  val spark = SparkUtil.createSpark("irisesClassification")
+//  val filePath = "./src/resources/irises/iris.data"
+//  val df = spark.read
+//    .format("csv")
+//    .option("inferSchema", "true")
+//    .load(filePath)
+//
+//  df.printSchema()
+//  df.describe().show(false)
+//  df.show(5)
+//
+//  val supervised = new RFormula()
+//    .setFormula("flower ~ . ") //so formulae is defined but not called here
+//
+//  val ndf = df.withColumnRenamed("_c4", "flower")
+//  ndf.show(5, false)
+//
+//  val fittedRF = supervised.fit(ndf)
+//  val preparedDF = fittedRF.transform(ndf)
+//  preparedDF.show(false)
+//  preparedDF.sample(0.1).show(false)
+//
+//  val Array(train, test) = preparedDF.randomSplit(Array(0.75, 0.25))
+//
+//  val randForest = new RandomForestRegressor()
+//    .setLabelCol("label")
+//    .setFeaturesCol("features")
+//
+//  val fittedModel = randForest.fit(train)
+//
+//  val testDF = fittedModel.transform(test)
+//
+//  testDF.show(30, false)
+//
+  def showAccuracy(df: DataFrame): Unit = {
+    val evaluator = new MulticlassClassificationEvaluator()
+      .setLabelCol("label")
+      .setPredictionCol("prediction")
+      .setMetricName("accuracy")
+    val accuracy = evaluator.evaluate(df)
+    println(s"DF size: ${df.count()} Accuracy $accuracy - Test Error = ${1.0 - accuracy}")
+  }
+//
+//  showAccuracy(testDF)
+
+  val spark = SparkUtil.createSpark("irisesClassification")
+  val filePath = "./src/resources/irises/iris.data"
+  val df = spark.read
+    .format("csv")
+    .option("inferSchema", "true")
+    .load(filePath)
+
+  df.printSchema()
+  df.describe().show(false)
+  df.show(5, false)
+
+  val supervised = new RFormula() //RFormula is a quicker way of creating needed column
+    .setFormula("flower ~ . ")
+
+  val ndf = df.withColumnRenamed("_c4", "flower")
+  ndf.show(5, false)
+
+  val fittedRF = supervised.fit(ndf)
+  val preparedDF = fittedRF.transform(ndf)
+  preparedDF.show(false)
+  preparedDF.sample(0.1).show(false)
+
+  val Array(train, test) = preparedDF.randomSplit(Array(0.75, 0.25))
+  val randomForest = new RandomForestClassifier()
+    .setLabelCol("label")
+    .setFeaturesCol("features")
+    .setNumTrees(10) //this is so called hyperparameter for this particular algorithm
+
+  val fittedModel = randomForest.fit(train) //create a model
+  val testDF = fittedModel.transform(test)  //use this model to make predictions and save them in a DataFrame
+  testDF.show(30,false)
+
+  showAccuracy((testDF))
 }
