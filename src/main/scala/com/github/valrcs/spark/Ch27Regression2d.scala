d3413463eef1e496d399fca6df2b75489ba12f69
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-07-17 11:06:19 +0300

    RFormula for specific columns

--- src/main/scala/com/github/valrcs/spark/Ch27Regression2d.scala
+++ src/main/scala/com/github/valrcs/spark/Ch27Regression2d.scala
@@ -13,4 +13,18 @@ object Ch27Regression2d extends App {
 
   df.printSchema()
   df.show(5)
+
+  import org.apache.spark.ml.feature.RFormula
+  val supervised = new RFormula()
+    .setFormula("y ~ x1 + x2 ") //so here y is label and x1 and x2 will be used as features
+//    .setFormula("y ~ . ") //so y is the answer and rest will be used as features
+
+  //if you do not need some column you could do it in RFormula as above line 19 or alternative
+  //would be to select the columns you want in DataFrame  before the below fit -> transform
+
+  val ndf = supervised
+    .fit(df) //prepares the formula
+    .transform(df) //generally transform will create the new data
+
+  ndf.show(5)
 }
