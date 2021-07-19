568ae40a8d25d5550b5427c20ea5f4a602b42478
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-07-19 21:16:22 +0300

    Laura's Solution to Jul 19 Exercise

--- src/main/scala/com/github/valrcs/spark/ExerciseJul19.scala
+++ src/main/scala/com/github/valrcs/spark/ExerciseJul19.scala
@@ -1,11 +1,34 @@
 package com.github.valrcs.spark
 
+import org.apache.spark.sql.functions.{col, lit, round}
+
 object ExerciseJul19 extends App {
   val spark = SparkUtil.createSpark("exerciseJul19")
-  val filePath = "./src/resource/csv/stocks_2013_2018.csv"
+  val filePath = "./src/resources/csv/stocks_2013_2018.csv"
 
   //TODO read the file, infer schema
 
   //TODO calculate the daily change in percentages for all rows
   //TODO group the results by day and calculate the average change for all stocks combined by day
+
+  val df = spark.read.format("csv")
+    .option("header", "true")
+    .option("inferSchema", "true")
+    .load(filePath)
+    .coalesce(5) //?
+
+  df.printSchema()
+  df.show(5, false)
+
+  //TODO calculate the daily change in percentages for all rows
+
+  val change = df.withColumn("change", round(lit(100) * (col("close") - col("open"))/col("open"), 2))
+  change.show()
+  //change in %, % sign not shown
+
+  //TODO group the results by day and calculate the average change for all stocks combined by day
+  change.groupBy("date").avg("change").show(false) //we should probably do round at the end
+  //if you do too much rounding in between you may actually start losing meaningful information
+
+
 }
