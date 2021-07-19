bf100efb331ab0bbd44ad881b26c9cc3f00e78f0
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-07-19 21:39:20 +0300

    Getting Previous Day Close and Open values

--- src/main/scala/com/github/valrcs/spark/ExerciseJul19.scala
+++ src/main/scala/com/github/valrcs/spark/ExerciseJul19.scala
@@ -1,6 +1,6 @@
 package com.github.valrcs.spark
 
-import org.apache.spark.sql.functions.{col, lit, round}
+import org.apache.spark.sql.functions.{avg, col, desc, expr, lit, round}
 
 object ExerciseJul19 extends App {
   val spark = SparkUtil.createSpark("exerciseJul19")
@@ -22,13 +22,39 @@ object ExerciseJul19 extends App {
 
   //TODO calculate the daily change in percentages for all rows
 
-  val change = df.withColumn("change", round(lit(100) * (col("close") - col("open"))/col("open"), 2))
-  change.show()
-  //change in %, % sign not shown
-
-  //TODO group the results by day and calculate the average change for all stocks combined by day
-  change.groupBy("date").avg("change").show(false) //we should probably do round at the end
+//  val change = df.withColumn("change", round(lit(100) * (col("close") - col("open"))/col("open"), 2))
+//  change.show()
+//  //change in %, % sign not shown
+//
+//  //TODO group the results by day and calculate the average change for all stocks combined by day
+//  change.groupBy("date").avg("change").show(false) //we should probably do round at the end
   //if you do too much rounding in between you may actually start losing meaningful information
 
+  val dfWithReturn = df
+    .withColumn("daily_return", (col("close") - col("open"))/col("open")*100)
 
+  dfWithReturn
+    .groupBy("date")
+//    .pivot("Name")
+    .agg(avg("daily_return"))
+//    .orderBy(desc("date"))
+    .orderBy(desc("avg(daily_return)"))
+    .show(10, false)
+
+  dfWithReturn.orderBy(desc("daily_return"))
+    .show(10, false) //days could be matching
+
+  dfWithReturn.orderBy(desc("daily_return"))
+    .where("Name != 'NFLX'")
+    .show(10, false) //days could be matching
+
+  val dfWithPreviousDay = df.withColumn("prevOpen", expr("" +
+    "LAG (open,1,0) " +
+    "OVER (PARTITION BY Name " +
+    "ORDER BY date )"))
+    .withColumn("prevClose", expr("" +
+      "LAG (close,1,0) " +
+      "OVER (PARTITION BY Name " +
+      "ORDER BY date )"))
+  dfWithPreviousDay.show(10, false)
 }
