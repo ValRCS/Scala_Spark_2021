db3b3d490767898106f34f267ce5cef4ac4fcf12
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-07-20 20:04:58 +0300

    Exercise 20 calculate walking distance

---
+++ src/main/scala/com/github/valrcs/spark/ExerciseJul20.scala
@@ -0,0 +1,17 @@
+package com.github.valrcs.spark
+
+object ExerciseJul20 extends App {
+  val spark = SparkUtil.createSpark("exerciseJul20")
+
+  //TODO read part-01.json
+  //TODO get schema
+  //TODO count how many walking events there were for user i
+  //TODO find out how far user i has walked :)
+  //TODO super simple would be find starting and ending location
+
+  //For those who are comfortable
+  //TODO even better would be if you can calculate distance between each walking event
+  //above would probably involve Window functios
+  // TODO this would involve sorting by Creation Time
+
+}
